84eb74179392c931c754ec604b7e35be630481fa
chuck-h <cfharr@gmail.com>
2012-12-10 21:47:20 -0800

    Strip out twi slave mode

--- twi.h
+++ twi.h
@@ -39,13 +39,9 @@
   #define TWI_STX   4
   
   void twi_init(void);
-  void twi_setAddress(uint8_t);
   uint8_t twi_readFrom(uint8_t, uint8_t*, uint8_t);
   int8_t twi_nonBlockingReadFrom(uint8_t address, uint8_t* data, uint8_t length);
   uint8_t twi_writeTo(uint8_t, uint8_t*, uint8_t, uint8_t);
-  uint8_t twi_transmit(const uint8_t*, uint8_t);
-  void twi_attachSlaveRxEvent( void (*)(uint8_t*, int) );
-  void twi_attachSlaveTxEvent( void (*)(void) );
   void twi_reply(uint8_t);
   void twi_stop(void);
   void twi_releaseBus(void);
